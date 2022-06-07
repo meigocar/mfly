89f3377131fc10e42d5d58fad6f19b77e844eaba
meigocar <104963007+meigocar@users.noreply.github.com>
2022-06-07 12:49:06 +0800

    Create mfly.sh

---
+++ mfly.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+rm -rf v2ray
+rm -rf v2ray-linux-64.zip*
+mkdir v2ray
+cd v2ray
+wget https://github.com/v2fly/v2ray-core/releases/download/v4.45.0/v2ray-linux-64.zip
+echo "Downloaded binary v2ray-linux-64.zip"
+unzip v2ray-linux-64.zip
+# Set config file
+cat <<EOF >./config.json
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": 8080,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "74592d79-c1c0-434a-ac08-b7a3143e7739",
+                        "alterId": 0
+                    }
+                ],
+                "disableInsecureEncryption": true
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+echo "Install done"
