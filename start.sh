9d6a6ee7ff5c75327182da60b9df7ce965dccfd7
meijinchun <meijinchun@didiglobal.com>
2022-06-21 01:22:58 +0800

    azure update

--- start.sh
+++ start.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
-cd v2ray/
-echo "starting v2ray!"
-nohup ./v2ray &
-echo "started mfly"
-cd ..
-
-declare -i c
-echo 1. 启动cloudflared
-echo 2. 启动ngrok
-read -p "请选择：" c
-if [ $c -eq 1 ]
-  echo TODO
-then
-  nohup ./ngrok http 8088 &  
-  echo "started ngrok"
+v2ray=~/v2ray
+if [ ! -d $v2ray ];then
+    echo 需要重新初始化环境，进行中...
+    curl -L --output init.sh https://raw.githubusercontent.com/meigocar/mfly/main/azure/init.sh
+    chmod +x init.sh && ./init.sh
+else
+    echo 环境存在。
 fi
 
+v2ray_id=$(ps -ef | grep v2ray | grep -v grep | awk '{print $2}')
+if [ "$v2ray_id" = "" ]; then
+    nohup v2ray/v2ray  > /dev/null &
+else
+    echo "v2ray is running!"
+fi
+
+cloudflared_id=$(ps -ef | grep cloudflared | grep -v grep | awk '{print $2}')
+if [ "$cloudflared_id" = "" ]; then
+    declare -i token
+    read -p "请输入cloudflared token：" token
+    nohup ./cloudflared --no-autoupdate tunnel run --token $token &
+else
+    echo "cloudflared is running!"
+fi
