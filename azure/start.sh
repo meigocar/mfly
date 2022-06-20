5d5f4f9b2f0e0b8aa402468db4c918542153697a
meijinchun <meijinchun@didiglobal.com>
2022-06-20 20:53:01 +0800

    azure update

--- azure/start.sh
+++ azure/start.sh
@@ -11,11 +11,15 @@ fi
 v2ray_id=$(ps -ef | grep v2ray | grep -v grep | awk '{print $2}')
 if [ "$v2ray_id" = "" ]; then
     nohup v2ray/v2ray &
+else
+    echo "v2ray is running!"
 fi
 
 cloudflared_id=$(ps -ef | grep cloudflared | grep -v grep | awk '{print $2}')
 if [ "$cloudflared_id" = "" ]; then
     nohup ./cloudflared --no-autoupdate tunnel run --token eyJhIjoiNmQxZjBjZDJiODhhMzQzOWYyOGY3ODI1OGYxNzg5NjAiLCJ0IjoiYjZlNmE2NjItOWYyMS00MGQwLWJiZWEtZDMzNDViYzE3MmY5IiwicyI6IjlKOS9FK250OGxKQ05HU0ZGRWFXMTk5bjFxdVRXMmlOc0JkR3NLQ1hUbGM9In0= &
+else
+    echo "cloudflared is running!"
 fi
 
 
